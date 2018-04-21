dd6ad18a52cf0942b0f659cec02e551143df005b
Adrian Carriger <hello@adriancarriger.com>
2018-04-20 19:58:12 -0500

    fix(404): use better grammar

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const NotFoundPage = () => (
   <div>
     <h1>You are here!</h1>
-    <h2>But nothing found for you #404</h2>
+    <h2>But nothing was found for you #404</h2>
   </div>
 );
 
