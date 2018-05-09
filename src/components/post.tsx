4423257e8e5901fd37b2fcc64087c4e9a11bb65a
Adrian Carriger <hello@adriancarriger.com>
2018-05-08 19:13:18 -0500

    feat(post): add post component

---
+++ src/components/post.tsx
@@ -0,0 +1,11 @@
+import Link from 'gatsby-link';
+import * as React from 'react';
+
+export default class extends React.Component<any, {}> {
+  constructor(props: any, context: any) {
+    super(props, context);
+  }
+  public render() {
+    return <span> - Post component</span>;
+  }
+}
